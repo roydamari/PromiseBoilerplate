cd110b7ff04b486c9b815501898723117f2ac019
roydamari <roydamari92@gmail.com>
2020-10-05 18:18:34 +0300

    solution

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-class Declare{
-  //implement here
+class Declare {
+
+  constructor(executor = () => { }) {
+    this.state = 'pending'
+    this.value
+    this.resolveHandler = () => { }
+    executor(this.resolve.bind(this))
+  }
+
+  resolve(value) {
+    if (this.state === 'pending') {
+      this.state = 'resolved';
+      this.value = value
+      this.resolveHandler(value)
+    }
+  }
+
+  then(resolveFunction = () => { }) {
+    this.resolveHandler = resolveFunction
+    if (this.state === 'resolved') {
+      const thenValue = resolveFunction(this.value)
+      const newDeclare = new Declare((newResolve) => {
+        newResolve(thenValue)
+      })
+      return newDeclare
+    } else {
+      return this
+    }
+  }
 }
 module.exports = Declare
